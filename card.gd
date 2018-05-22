bfa4ed65fd4d30181a873651757502894b809ec0
LukNeum <lukasneumann93@web.de>
2018-05-22 23:41:58 +0200

    Initial Commit

---
+++ card.gd
@@ -0,0 +1,299 @@
+extends Node2D
+
+var type = 0
+var value = 1
+var size = Vector2(80,120)
+var location = Vector2(-1,-1) # -1, -1 refers to the initial position on the deck
+var selected = false
+var colour = Color(1,1,1,1)
+var game
+var outline_thickness = 3
+var substack = [self]
+var moving = false
+var in_temp_storage = 0 # int from 0 - 3, indicating which temp slot is occupied by this card
+var in_final_storage = false
+
+# cosmetics
+var animating = false
+var animation_timer = 0
+var animation_time = 0
+var old_position = Vector2(0,0)
+var desired_position = Vector2(0,0)
+
+const RED = Color(1,0.7,0.7,1)
+const GREEN = Color(0.7,1,0.7,1)
+const BLUE = Color(0.7,0.7,1,1)
+const WHITE = Color(1,1,1,1)
+const TYPECOLOURS = [RED,GREEN,BLUE,WHITE]
+
+var label
+var big_label
+
+
+func _ready():
+	# initialise labels
+	var font = preload("res://fonts/new_dynamicfont.tres")
+	var font2 = preload("res://fonts/new_dynamicfont2.tres")
+	font2.set_size(30)
+	font.set_size(80)
+	label = Label.new()
+	big_label = Label.new()
+	label.text = str(value)
+	big_label.text = str(value)
+	big_label.rect_position = game.card_size/4
+	z_index = 0
+	if value == 0:
+		label.text = "ACE"
+		big_label.text = "A"
+	if type == 3:
+		label.text = "JOKER"
+		big_label.text = "J"
+	label.add_color_override("font_color", Color(0,0,0))
+	label.add_font_override("font", font2)
+	big_label.add_color_override("font_color", Color(0,0,0))
+	big_label.add_font_override("font", font)
+	add_child(label)
+	add_child(big_label)
+	position = game.get_deck_position()
+	
+	set_process_input(true)
+	set_process(true)
+
+func _process(delta):
+	if animating:
+		game.somethings_animating = true
+		animation_timer += delta
+		interpolate_position()
+
+func _input(event):
+	if game.accepts_input:
+		# pressing LMB
+		if Input.is_action_just_pressed("ui_select"):
+			if is_hovering():
+				get_substack()
+				if can_move():
+					moving = true
+					
+					# if card was in temp storage, free up slot while moving
+					if in_temp_storage:
+						game.temp_storage[in_temp_storage -1] = null
+		# letting go of LMB
+		if !Input.is_action_pressed("ui_select"):
+			if moving:
+				# check if stack has been moved to another stack
+				for stack in game.stack:
+					for card in stack:
+						if card != self:
+							if card.is_in_area(get_center_global()):
+								if can_stack(card,self):
+									set_location(card.location + Vector2(0,1))
+									moving = false
+									break
+				# is there an empty stack it can be moved to?
+				for i in range(game.stack.size()):
+					if game.stack[i].size() == 0:
+						if game.point_is_in_stack_base(get_center_global(),i):
+							set_location(Vector2(i,0))
+							break
+				# can it be moved to storage?
+				if substack.size() == 1:
+					var temp_slot = game.position_in_temp(get_center_global())
+					if temp_slot != null:
+						move_to_temp_storage(temp_slot)
+					var final_slot = game.position_in_final(get_center_global())
+					if final_slot != null:
+						move_to_final_storage(final_slot)
+				# move back to old location
+				if in_temp_storage:
+					move_to_temp_storage(in_temp_storage - 1)
+				else:
+					set_location(location)
+				moving = false
+				game.recheck()
+		# moving mouse
+		if event is InputEventMouseMotion:
+			if moving:
+				translate_stack(event.relative)
+
+
+func _draw():
+	draw_rect(Rect2(Vector2(-outline_thickness,-outline_thickness),game.card_size + Vector2(2,2) * outline_thickness), Color(0,0,0,1))
+	draw_rect(Rect2(Vector2(0,0),game.card_size),colour)
+
+func set_location(new_loc, animated = false, from_position = position, duration = 0.1, recheckneeded = false):
+	moving = false
+	if recheckneeded:
+		#get_substack()
+		pass
+	for i in range(substack.size()):
+		var card = substack[i]
+		# check if card is in final
+		if card.in_final_storage == false:
+			# remove card from old stack
+			game.remove_from_stack(card,card.location.x)
+			card.location = new_loc + Vector2(0,1)*(i)
+			# and add to new stack
+			game.append_to_stack(card,card.location.x)
+			var new_position = Vector2(card.location.x * (game.card_width + game.stack_spacing) + game.card_width / 2, card.location.y * game.stacked_offset + game.top_offset)
+			if animated:
+				start_animation(from_position, new_position, duration)
+			else:
+				card.position = new_position
+			card.z_index = card.location.y
+			in_temp_storage = 0
+
+func move_to_temp_storage(slot):
+	# check if slot is free
+	if game.temp_storage[slot] == null:
+		# remove card from old stack
+		game.remove_from_stack(self,location.x)
+		in_temp_storage = slot + 1
+		game.temp_storage[slot] = self
+		location = Vector2(-slot,0)
+		position = game.get_temp_slot_position(slot)
+		z_index = 1
+
+func move_to_final_storage(slot, animated = true, duration = 0.5):
+	# check if slot is of correct type
+	if slot == type:
+		# check if condition is true
+		var move = false
+		if game.final_storage[slot] == null:
+			if  value == 1:
+				move = true
+		elif value == game.final_storage[slot].value + 1: 
+			move = true
+		if move:
+			# disable mouse dragging
+			moving = false
+			if in_temp_storage:
+				game.temp_storage[in_temp_storage - 1] = null
+				in_temp_storage = 0
+			#remove from old stack
+			game.remove_from_stack(self, location.x)
+			in_final_storage = true
+			game.final_storage[slot] = self
+			location = Vector2(-2,0)
+			if animated:
+				start_animation(position, game.get_final_slot_position(slot), duration)
+			else:
+				position = game.get_final_slot_position(slot)
+			z_index = value + 15
+			return true
+	else:
+		return false
+		
+
+func remove_joker(animated = true, duration = 0.5):
+	if type == 3:
+		# remove from stack
+		game.remove_from_stack(self,location.x)
+		# move to joker pos
+		if animated:
+			start_animation(position,game.get_final_slot_position(-1), duration )
+		else:
+			position = game.get_final_slot_position(-1)
+		# lock card
+		in_final_storage = true
+		return true
+
+func remove_ace(slot):
+	if value == 0:
+		# remove from old stack
+		game.remove_from_stack(self,location.x)
+		# or temp if in temp
+		if in_temp_storage:
+			game.temp_storage[in_temp_storage -1] = null
+		# move card to temp slot and set final with a neat animation
+		start_animation(position, game.get_temp_slot_position(slot), 0.3)
+		game.temp_storage[slot] = self
+		location = Vector2(-slot,0)
+		in_final_storage = true
+
+func translate_stack(translation):
+	for i in range(substack.size()):
+		if substack[i].in_final_storage == false:
+			substack[i].z_index = i + 10
+			substack[i].position += translation
+
+func set_type(new_type):
+	if typeof(new_type) == TYPE_INT and new_type <= 3 and new_type >= 0:
+		type = new_type
+		set_colour(TYPECOLOURS[new_type])
+
+func set_value(new_value):
+	if typeof(new_value) == TYPE_INT and new_value <= 9 and new_value >= 0:
+		value = new_value
+
+func is_hovering():
+	var local_pos = get_global_mouse_position() - position
+	var checksize = game.card_size
+	if !is_uppermost():
+		checksize = Vector2(game.card_size.x,game.stacked_offset)
+	return (local_pos.x > 0 and local_pos.x <= checksize.x and local_pos.y > 0 and local_pos.y <= checksize.y)
+
+func is_in_area(global_position):
+	var local_pos = global_position - position
+	var checksize = game.card_size
+	if !is_uppermost():
+		checksize = Vector2(game.card_size.x,game.stacked_offset)
+	if(local_pos.x > 0 and local_pos.x <= checksize.x and local_pos.y > 0 and local_pos.y <= checksize.y):
+		return self
+	else:
+		return false
+
+func get_center_global():
+	return Vector2(game.card_size/2 + position)
+
+func set_colour(new_colour):
+	if typeof(new_colour) == TYPE_COLOR:
+		colour = new_colour
+		update()
+
+func is_uppermost():
+	if game:
+		if in_final_storage:
+			return false
+		if game.stack.size():
+			if game.stack[location.x].size() - 1 == location.y:
+				return true
+		if in_temp_storage:
+			return true
+
+func interpolate_position():
+	position = old_position + (desired_position - old_position) * (animation_timer / animation_time)
+	if animation_timer >= animation_time:
+		animating = false
+		animation_timer = 0
+		position = desired_position
+
+func start_animation(from_position, to_position, duration):
+	animating = true
+	old_position = from_position
+	desired_position = to_position
+	animation_time = duration
+
+func can_move():
+	if in_final_storage:
+		return false
+	if is_uppermost():
+		return true
+	else:
+		if get_substack():
+			return true
+	return false
+
+func can_stack(card1, card2):
+	if card1.value == card2.value +1 and card1.type != card2.type and card2.value >= 1:
+		return true
+
+func get_substack():
+	substack = [self]
+	if !in_temp_storage:
+		for i in range(location.y + 1, game.stack[location.x].size()):
+			if can_stack(substack.back(), game.stack[location.x][i]):
+				substack.append(game.stack[location.x][i])
+			else:
+				substack = []
+				return false
+	return true
---
+++ game.gd
@@ -0,0 +1,289 @@
+extends Node
+
+# this node creates the deck, shuffles the cards and keeps track of the individual stacks and handles the "dragon" buttons and behaviour
+var deck = []
+var stack = []
+var cards = []
+var aces = [[],[],[]]
+var temp_storage = [null,null,null]
+var final_storage = [null,null,null]
+var canremove = [false,false,false]
+
+# rechecking
+var recheck_requested
+
+# meta stuff
+var accepts_input = false
+onready var seed_input = get_node("LineEdit")
+onready var seed_display = get_node("Label")
+
+# animation stuff
+var somethings_animating = false
+var shuffling = false
+var shuffling_stack = 0
+
+# location parameters
+var stacked_offset = 20
+var stack_spacing = 30
+var top_offset = 160
+var card_width = 80
+var card_height = 120
+var screen_size = Vector2()
+var card_size = Vector2()
+var scale
+
+# buttons
+onready var buttonred = get_node("Button")
+onready var buttongreen = get_node("Button2")
+onready var buttonblue = get_node("Button3")
+
+onready var win_label = get_node("Label2")
+
+
+func _ready():
+	get_tree().connect("screen_resized", self, "_on_screen_resized")
+	var newseed = rand_seed(OS.get_unix_time())[1]
+	seed(newseed)
+	seed_display.text = "Current seed: " + str(newseed)
+	update_sizes()
+	update_button_visibility()
+	# initialise stacks
+	stack = [[],[],[],[],[],[],[],[]]
+	generate_deck()
+	shuffling = true
+	update()
+	set_process(true)
+	set_process_input(true)
+
+func reset_game(newseed = OS.get_unix_time()):
+	accepts_input = false
+	win_label.visible = false
+	seed(newseed)
+	seed_display.text = "Current seed: " + str(newseed)
+	# clear board
+	for card in cards:
+		card.queue_free()
+	for card in deck:
+		card.queue_free()
+	# reset lists
+	deck = []
+	stack = [[],[],[],[],[],[],[],[]]
+	cards = []
+	aces = [[],[],[]]
+	shuffling_stack = 0
+	temp_storage = [null,null,null]
+	final_storage = [null,null,null]
+	canremove = [false,false,false]
+	generate_deck()
+	shuffling = true
+
+func _process(delta):
+	if !somethings_animating:
+		if recheck_requested:
+			recheck()
+		if shuffling:
+			if deck.size():
+				var card_index = randi() % deck.size()
+				var shuffeled_card = deck[card_index]
+				deck.remove(card_index)
+				cards.append(shuffeled_card)
+				shuffeled_card.z_index = deck.size()
+				shuffeled_card.set_location(Vector2(shuffling_stack,stack[shuffling_stack].size()), true, get_final_slot_position(-1) ,0.05)
+				shuffling_stack += 1
+				if shuffling_stack > stack.size() - 1:
+					shuffling_stack = 0
+			if deck.size() == 0:
+				shuffling = false
+				accepts_input = true
+				recheck()
+		
+		
+	somethings_animating = false
+
+
+func generate_deck():
+	# generate all cards
+	var card_class = preload("res://card.gd")
+	for type in range(0,3): # for all suits
+		for value in range(1,10):
+			var new_card = card_class.new()
+			new_card.set_type(type)
+			new_card.set_value(value)
+			new_card.game = self
+			add_child(new_card)
+			deck.append(new_card)
+		for dragon in range(0,4):
+			var new_card = card_class.new()
+			new_card.set_type(type)
+			new_card.set_value(0)
+			new_card.game = self
+			add_child(new_card)
+			deck.append(new_card)
+	# and add the "joker"
+	var new_card = card_class.new()
+	new_card.set_type(3)
+	new_card.set_value(0)
+	new_card.game = self
+	add_child(new_card)
+	deck.append(new_card)
+
+func update_sizes():
+	# set proportions
+	screen_size = OS.get_real_window_size()
+	scale = 1920.0 / screen_size.x
+	card_width = screen_size.x / 13
+	card_height = screen_size.y / 5
+	card_size = Vector2(card_width,card_height)
+	stacked_offset = screen_size.y / 20
+	stack_spacing = card_width / 2
+	top_offset = card_height * 1.5
+	
+func update_button_visibility():
+	buttonred.visible = canremove[0]
+	buttongreen.visible = canremove[1]
+	buttonblue.visible = canremove[2]
+
+func append_to_stack(card, stackindex):
+	stack[stackindex].append(card)
+
+func remove_from_stack(card, stackindex):
+	if stack[stackindex].has(card):
+		stack[stackindex].erase(card)
+
+func position_in_temp(position):
+	for i in range(temp_storage.size()):
+		if Rect2(Vector2((card_width + stack_spacing) * (i + 1), stacked_offset), Vector2(card_width,card_height)).has_point(position):
+			return i
+
+func move_to_temp(card,slot):
+	if temp_storage[slot] == null:
+		temp_storage[slot] = card
+
+
+func position_in_final(position):
+	for i in range(final_storage.size()):
+		if Rect2(Vector2((card_width + stack_spacing) * (i + 5), stacked_offset), Vector2(card_width,card_height)).has_point(position):
+			return i
+
+func get_deck_position():
+	return get_final_slot_position(-1)
+
+func get_temp_slot_position(slot):
+	return Vector2((card_width + stack_spacing) * (slot + 1), stacked_offset)
+
+func get_final_slot_position(slot):
+	return Vector2((card_width + stack_spacing) * (slot + 5), stacked_offset)
+
+func point_is_in_stack_base(point,stackindex):
+	return Rect2(Vector2((card_width + stack_spacing) * (stackindex) + card_width / 2, top_offset), Vector2(card_width,card_height)).has_point(point)
+
+func _draw():
+	# draw temp storage outlines
+	for i in range(temp_storage.size()):
+		draw_rect(Rect2(Vector2((card_width + stack_spacing) * (i + 1), stacked_offset), Vector2(card_width,card_height)),Color(1,1,1,1),false)
+	# draw final storage outlines
+	for i in range(final_storage.size()):
+		var color = Color(0,0,0,1)
+		color[i] = 1
+		draw_rect(Rect2(Vector2((card_width + stack_spacing) * (i + 5), stacked_offset), Vector2(card_width,card_height)),color,false)
+
+func lowest_final():
+	var lowest = 10
+	for i in range(final_storage.size()):
+		if final_storage[i] != null:
+			lowest = min(lowest, final_storage[i].value)
+		else:
+			lowest = 0
+	return lowest
+
+func recheck():
+	# check if cards can be moved to final and if aces can be removed
+	recheck_requested = false
+	var aces_local = [[],[],[]]
+	var lowest_value = lowest_final()
+	for card in cards:
+		if card.is_uppermost():
+			if (card.value <= 2 or card.value == lowest_value + 1 ) and card.type <= 2:
+				if card.move_to_final_storage(card.type):
+					recheck_requested = true
+					somethings_animating = true
+					break
+			if card.type == 3:
+				if card.remove_joker():
+					recheck_requested = true
+					somethings_animating = true
+					break
+			# check if aces can be removed
+			if card.value == 0 and card.type <= 2:
+				aces_local[card.type].append(card)
+	for type in range(aces_local.size()):
+		if aces_local[type].size() == 4:
+			# check if there is an available temp slot
+			canremove[type] = false
+			for i in range(temp_storage.size()):
+				if temp_storage[i] == null:
+					canremove[type] = true
+				elif temp_storage[i].type == type and temp_storage[i].value == 0:
+					canremove[type] = true
+		else:
+			canremove[type] = false
+		# move aces to global scope
+		aces = aces_local
+	update_button_visibility()
+	# win condition
+	var remaining_cards = 0
+	for individual_stack in stack:
+		for card in individual_stack:
+			remaining_cards += 1
+	if remaining_cards == 0:
+		print("You Win!")
+		win_label.visible = true
+
+func _on_screen_resized():
+	update_sizes()
+
+func _input(event):
+	if event.is_action("restart"):
+		get_tree().reload_current_scene()
+	if event.is_action("ui_cancel"):
+		get_tree().quit()
+
+func remove_aces(type):
+	var slot = - 1
+	# find where to put them
+	for i in range(temp_storage.size()):
+		if temp_storage[i] != null:
+			if temp_storage[i].type == type and temp_storage[i].value == 0:
+				slot = i
+				break
+	# check again if empty temp slots available (because occupied slots are preferred (cosmetic)
+	if slot == -1:
+		for i in range(temp_storage.size()):
+			if temp_storage[i] == null:
+				slot = i
+				break
+	for card in aces[type]:
+		card.remove_ace(slot)
+	recheck_requested = true
+	#recheck()
+
+func _on_Button_button_up():
+	# remove red aces
+	remove_aces(0)
+
+
+func _on_Button2_button_up():
+	# remove green aces
+	remove_aces(1)
+
+
+func _on_Button3_button_up():
+	# remove blue aces
+	remove_aces(2)
+
+
+func _on_Button4_button_up():
+	var input = int(seed_input.text)
+	if input == 0:
+		input = rand_seed(OS.get_unix_time())[1]
+	reset_game(input)
